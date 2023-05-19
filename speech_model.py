108945f4346a2deaa68104e10e291b2ea2089200
Manikandan <64252570+tonystark3110@users.noreply.github.com>
2023-05-19 15:32:21 +0530

    Update speech_model.py

--- speech_model.py
+++ speech_model.py
@@ -1,24 +1,3 @@
-#
-#
-#from vosk import Model , KaldiRecognizer
-#import pyaudio
-#
-#model = Model('C:\\Users\\20325730\\Desktop\\speech_rec\\vosk-model-en-us-0.22')
-#recognizer = KaldiRecognizer(model , 16000)
-#
-#cap = pyaudio.PyAudio()
-#stream = cap.open (format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
-#stream.start_stream()
-#
-#while True:
-#    data = stream.read(4096)
-#    if len(data) == 0:
-#        break
-#
-#    if recognizer.AcceptWaveform(data):
-#        print(recognizer.Result())
-
-
 from vosk import Model, KaldiRecognizer
 import pyaudio
 import json
@@ -29,6 +8,21 @@ recognizer = KaldiRecognizer(model, 16000)
 cap = pyaudio.PyAudio()
 stream = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
+
+# Dictionary to map words to their replacements
+word_replacements = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+    # Add more word replacements as needed
+}
 
 print("Listening...")
 
@@ -41,11 +35,17 @@ while True:
         result = recognizer.Result()
         result_json = json.loads(result)
         recognized_text = result_json["text"]
+
+        # Convert recognized words to integers and apply word replacements
+        recognized_numbers = recognized_text.split()
+        for i, word in enumerate(recognized_numbers):
+            if word.isdigit():
+                recognized_numbers[i] = str(int(word))
+            elif word in word_replacements:
+                recognized_numbers[i] = word_replacements[word]
+
+        # Print the recognized text with spaces
+        recognized_text = " ".join(recognized_numbers)
         print(recognized_text)
         break
 
-# Stop the audio stream
-stream.stop_stream()
-stream.close()
-cap.terminate()
-       
