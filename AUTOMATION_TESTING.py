9bad4a0fa1c5ebe12baabdc9a6cf1272d8bc91d3
Manikandan <64252570+tonystark3110@users.noreply.github.com>
2023-06-20 11:40:48 +0530

    Update AUTOMATION_TESTING.py

--- AUTOMATION_TESTING.py
+++ AUTOMATION_TESTING.py
@@ -14,62 +14,10 @@ import json
 from vosk import Model, KaldiRecognizer
 from playsound import playsound
 r = sr.Recognizer()
-
-
-
-audio_file = 'C:\\Users\\20325730\\Desktop\\PROJECT\\yes_no.mp3'
-
-#model = Model('C:\\Users\\20325730\\Desktop\\PROJECT\\vosk-model-en-us-0.42-gigaspeech')
-model = Model('C:\\Users\\20325730\\Desktop\\PROJECT\\vosk-model-en-in-0.5')
+audio_file = 'C:\\Users\\20325730\\Desktop\\PROJECT\\PROJECT\\yes_no.mp3'
+model = Model('C:\\Users\\20325730\\Desktop\\PROJECT\\PROJECT\\vosk-model-en-in-0.5')
 recognizer = KaldiRecognizer(model, 16000)
-
-
-word_replacements = {
-    "zero": "0",
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9",
-    "eye": "i",
-    "ten": "10",
-    "eleven": "11",
-    "twelve": "12",
-    "thirteen": "13",
-    "fourteen": "14",
-    "fifteen": "15",
-    "sixteen": "16",
-    "seventeen": "17",
-    "eighteen": "18",
-    "nineteen": "19",
-    "twenty": "20",
-    "thirty": "30",
-    "forty": "40",
-    "fifty": "50",
-    "sixty": "60",
-    "seventy": "70",
-    "eighty": "80",
-    "ninety": "90",
-    "tree": "3",
-    "dot": ".",
-    "underscore": "_",
-    "dollar": "$",
-    "hash": "#",
-    "star": "*",
-    "plus": "+",
-    "minus": "-",
-    #"space": "",
-    "dash": "-",
-    "comma": ",",
-    "and" : "&"
-}
-
-
-
+word_replacements = {"zero": "0",}
 # Returning the value of input
 def perform_speech_recognition():
  try:
@@ -120,28 +68,22 @@ def input_entry(driver, xpath):
         try:
             text_2 = perform_speech_recognition()
             if not text_2:
-                # No voice input detected
                 continue
-
             text_bar = driver.find_element(By.XPATH, xpath)
             text_bar.clear()
             text_bar.send_keys(text_2)
             elements = driver.find_elements(By.XPATH, '(//span[@class="mat-option-text"]//span)')
-
             option_found = False
-
             for element in elements:
                 if text_2.lower() in element.text.lower():
                     element.click()
                     option_found = True
                     break
-
             if not option_found:
                 print("Invalid input detected:", text_2)
                 text_bar.clear()
                 print("Retrying...")
                 continue
-
             while True:
                 # Prompt the user to confirm the selected option
                 playsound(audio_file)
@@ -159,25 +101,17 @@ def input_entry(driver, xpath):
 
                 print("Invalid input. Please enter 'yes' or 'no'.")
                 playsound(audio_file)
-
         except Exception as e:
             print("An error occurred:", str(e))
             print("Retrying...")
             continue
-
-
-
-
 url = "https://eip4dev.lntecc.com/EIPSCMUI/SOPUI/indent-request/indentReq"
-chrome_driver_path = "C:\\Users\\20325730\\Desktop\\PROJECT\\chromedriver_win32\\chromedriver.exe"
+chrome_driver_path = "C:\\Users\\20325730\\Desktop\\PROJECT\\PROJECT\\chromedriver_win32\\chromedriver.exe"
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 driver.get(url)
-#driver.maximize_window()
+driver.maximize_window()
 chrome_options = Options()
-#chrome_options.add_argument("--headless")  # Optional: Run the browser in headless mode
-
-
 other_user = driver.find_element(By.XPATH,'/html/body/app-root/div/div[2]/app-login/div/form/div/div/div[1]/div/div[2]/div[1]/div/div[1]/ul/li[2]/a')
 other_user.click()
 username = driver.find_element(By.XPATH, '//*[@id="username"]')
@@ -200,39 +134,28 @@ create_indent.click()
 time.sleep(2)
 job = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[1]/app-indent-create-header/div/mat-accordion/mat-expansion-panel[1]/div/div/div/div[1]/div/eipautocomplete/mat-form-field/div/div[1]/div/input')
 job.click()
-
-
 xpath1 = '//*[@id="ActxtboxINDCJob"]'
 input_entry(driver, xpath1)
 time.sleep(1)
-
 xpath2 = '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[1]/app-indent-create-header/div/mat-accordion/mat-expansion-panel[1]/div/div/div/div[2]/div/eipautocomplete/mat-form-field/div/div[1]/div/input'
 input_entry(driver, xpath2)
 time.sleep(2)  
-
 stock_job = driver.find_element(By.XPATH, '//*[@id="ActxtboxINDCAccountingCentre"]')
 stock_job.click()
 pyautogui.press('backspace')
 pyautogui.press('backspace')
-
 xpath3 = '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[1]/app-indent-create-header/div/mat-accordion/mat-expansion-panel[1]/div/div/div/div[3]/div/eipautocomplete/mat-form-field/div/div[1]/div/input'
 input_entry(driver, xpath3)
 time.sleep(3)
-
 indent_type = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[1]/app-indent-create-header/div/mat-accordion/mat-expansion-panel[2]/div/div/div/div[1]/eipautocomplete/mat-form-field/div/div[1]/div/input')
 indent_type.click()
-
 xpath4 = '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[1]/app-indent-create-header/div/mat-accordion/mat-expansion-panel[2]/div/div/div/div[1]/eipautocomplete/mat-form-field/div/div[1]/div/input'
 input_entry(driver, xpath4)
-
 issue_type = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[1]/app-indent-create-header/div/mat-accordion/mat-expansion-panel[2]/div/div/div/div[2]/eipautocomplete/mat-form-field/div/div[1]/div/input')
 issue_type.click()
-
 xpath5 = '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[1]/app-indent-create-header/div/mat-accordion/mat-expansion-panel[2]/div/div/div/div[2]/eipautocomplete/mat-form-field/div/div[1]/div/input'
 input_entry(driver, xpath5)
-
 time.sleep(2) 
-
 #priority = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[1]/app-indent-create-header/div/mat-accordion/mat-expansion-panel[2]/div/div/div/div[3]/eipautocomplete/mat-form-field/div/div[1]/div/input')
 #priority.click()                          
 #time.sleep(1)
@@ -248,10 +171,8 @@ time.sleep(1)
 material_group = driver.find_element(By.XPATH, '/html/body/div[2]/div[4]/div/mat-dialog-container/app-general-cart-popup/div[2]/div[1]/eipautocomplete/mat-form-field/div/div[1]/div/input')
 time.sleep(1)
 material_group.click()
-
 xpath6 = '/html/body/div[2]/div[4]/div/mat-dialog-container/app-general-cart-popup/div[2]/div[1]/eipautocomplete/mat-form-field/div/div[1]/div/input'
 input_entry(driver, xpath6)
-
 time.sleep(1)
 select  = driver.find_element(By.XPATH, '/html/body/div[2]/div[4]/div/mat-dialog-container/app-general-cart-popup/div[2]/div[2]/eipgeneralcart/div/div/div[1]/div/div[2]/mat-selection-list/mat-list-option/div/div[2]')
 select.click()
@@ -264,29 +185,23 @@ post.click()
 time.sleep(2)                     
 HSN = driver.find_element(By.XPATH, ' /html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr/td[3]/eipautocomplete/mat-form-field/div/div[1]/div/input')
 HSN.click()
-
 xpath7 = '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr/td[3]/eipautocomplete/mat-form-field/div/div[1]/div/input'
 input_entry(driver, xpath7)
 time.sleep(1)
-
 time.sleep(2)
 tax_type = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr[1]/td[4]/div/eipautocomplete/mat-form-field/div/div[1]/div/input')
 tax_type.click()
-
 xpath8 =  '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr/td[4]/div/eipautocomplete/mat-form-field/div/div[1]/div/input'
 input_entry(driver, xpath8)
-
 expand_list = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr/td[10]/div/i')
 expand_list.click()
 cost_package = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr[2]/td[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr/td[1]/kendo-combobox/span/kendo-searchbar/input')
 cost_package.click()
 time.sleep(1)
-
 xpath9 =  '/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr[2]/td[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr/td[1]/kendo-combobox/span/kendo-searchbar/input'
 input_entry(driver, xpath9)
 time.sleep(2)
 press('enter')
-
 time.sleep(1)
 indent_qty = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[2]/div/div/mat-horizontal-stepper/div[2]/div[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr[2]/td[2]/div/kendo-grid/div/kendo-grid-list/div/div[1]/table/tbody/tr/td[4]/mat-form-field/div/div[1]/div/input')
 indent_qty.click()
@@ -298,13 +213,10 @@ while True:
     except Exception as e:
         print("An error occurred:", str(e))
         print("Retrying...")
-
-
 time.sleep(1)
 next_next = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[3]/div/div/button[3]')
 next_next.click()
 time.sleep(2)
 submit = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/mat-dialog-container/app-indent-create/div[3]/div/div/button[3]')
 submit.click()
-
 time.sleep(500)
